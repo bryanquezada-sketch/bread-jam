f54186036c122367f739eb843c845df00679421d
bryanquezada-sketch <bryan.quezada1@outlook.com>
2026-02-25 12:52:05 -0500

    Created Zones + debugger

--- src/game/scenes/Game.js
+++ src/game/scenes/Game.js
@@ -16,5 +16,45 @@ export class Game extends Scene
         this.physics.world.setBounds(0,0, 640, 360);
 
 //        this.add.image(512, 384, 'background').setAlpha(0.5);
+
+        const tileSize = 128;
+
+        const lot = [
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+        ];
+
+        this.add.zone();
+
+        for (let y = 0; y < lot.length; y++) {
+            for (let x = 0; x < lot.length; x++) {
+
+                let screenX = x * tileSize;
+                let screenY = y * tileSize;
+
+                if (lot[y][x] === 0) {
+                    this.add.zone(screenX, screenY, 128, 128);
+                    
+                }
+            }
+        }
+        
+        const debugGraphics = this.add.graphics();
+        this.children.list.forEach(child => {
+            if (child.type === 'Zone') {
+                debugGraphics.lineStyle(2, 0x00ff00).strokeRectShape(child.getBounds());
+            }
+        });
+
+        /// --- END OF CREATE ---
     }
+
+    update()
+    {
+
+    }
+
 }
