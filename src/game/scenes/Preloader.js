0e112eeb1d5f1521e42af60e86c169e59d85d733
bryanquezada-sketch <bryan.quezada1@outlook.com>
2026-02-26 22:50:07 -0500

    loaded some assets

--- src/game/scenes/Preloader.js
+++ src/game/scenes/Preloader.js
@@ -62,6 +62,16 @@ export class Preloader extends Scene
         this.load.image('b18', 'sourdough.jpg');
         this.load.image('b19', 'swiss.jpg');
 
+        this.load.spritesheet('fireball', 'fireballSpriteSheet.png'),{
+            frameWidth: 32,
+            frameHeight: 32
+        }
+
+        this.load.spritesheet('pidgeon', 'pidgeonSpriteSheet.png'),{
+            frameWidth: ,
+            frameHeight: 
+        }
+
         this.load.spritesheet('buildings', 'Building_SpriteSheet.png', {
             frameWidth: 128,
             frameHeight: 128
